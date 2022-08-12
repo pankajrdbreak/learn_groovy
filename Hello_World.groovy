d47653301435b88fd43ec6f436baa61dba8086c3
Pankaj Vare <76647860+pankajrdbreak@users.noreply.github.com>
2022-08-12 11:20:36 +0530

    Update Hello_World.groovy

--- Hello_World.groovy
+++ Hello_World.groovy
@@ -1,4 +1,6 @@
-def call(String name,String day){
-def call(Map config = [:]){
-sh "echo Hello ${config.name}. Today is ${config.day}"
+def call(){
+//def call(String name,String day){
+//def call(Map config = [:]){
+//sh "echo Hello ${config.name}. Today is ${config.day}"
+  sh "echo Hello World"
 }
